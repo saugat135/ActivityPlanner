67ae0a2478a602818f8b41d5f0c674dadf7e4f12
saugat135 <saugatbizid@gmail.com>
2016-06-25 03:13:19 +0545

    Create weekly activity file for creating different numbers of activities in each day of the week

--- ActivityPlanner/Utilities.swift
+++ ActivityPlanner/Utilities.swift
@@ -1,2 +1,16 @@
 import Foundation
 
+struct DateComponents {
+  
+  static func sharedDateComponents() -> NSDateComponents {
+    let dateComponents = NSDateComponents()
+    dateComponents.calendar = self.sharedCalendar()
+    return dateComponents
+  }
+  
+  private static func sharedCalendar() -> NSCalendar? {
+    return NSCalendar(identifier: NSCalendarIdentifierGregorian)
+  }
+  
+}
+
