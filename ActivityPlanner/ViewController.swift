117eeabbd01a1903a8cf4f9880c565e7f2c0b0b9
saugat135 <saugatbizid@gmail.com>
2016-06-24 16:01:36 +0545

    Initial Commit

---
+++ ActivityPlanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ActivityPlanner
+//
+//  Created by Saugat Gautam on 6/24/16.
+//  Copyright © 2016 Saugat Gautam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
